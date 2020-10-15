f9e8a121f3623720881cbcfcb7fa4225853d3fa8
Ganesh <jejurkarganesh5>
2020-10-15 18:58:43 +0530

    swagger documentation added

--- imdbapi/urls.py
+++ imdbapi/urls.py
@@ -4,10 +4,10 @@ from django.urls import path,include
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='IMDB MOVIE API')
+swagger_view = get_swagger_view(title='IMDB MOVIE API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'swagger/', schema_view),
+    url(r'swagger/', swagger_view),
     path('api/', include('movies.urls'))
 ]
--- movies/views.py
+++ movies/views.py
@@ -5,6 +5,26 @@ from .models import Movie, Genre
 from rest_framework.permissions import AllowAny, IsAdminUser
 
 class MovieOperations(ModelViewSet):
+
+    """
+        retrive:
+          return a movie instance.
+
+        list:
+            Return all movies, ordered by mostly joined.
+
+        create:
+            Create a new movie
+
+        delete:
+            Delete an existing movie.
+
+        partial_update:
+            Update one or more fields on an existing movie.
+
+        update:
+            Update a movie.
+    """
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
@@ -14,6 +34,27 @@ class MovieOperations(ModelViewSet):
         return super().get_permissions()
 
 class GenreOperations(ModelViewSet):
+
+    """
+        retrive:
+          return a Genre instance.
+
+        list:
+            Return all Genre, ordered by mostly joined.
+
+        create:
+            Create a new Genre.
+
+        delete:
+            Delete an existing Genre.
+
+        partial_update:
+            Update one or more fields on an existing Genre.
+
+        update:
+            Update a Genre.
+    """
+
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
